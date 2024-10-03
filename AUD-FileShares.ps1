eb31d38120781f16c2de0c4e6a8cfb172ba9cf47
Leon Moris <67964202+LeonMoris@users.noreply.github.com>
2024-10-03 08:38:02 +0200

    Create AUD-FileShares.ps1

---
+++ AUD-FileShares.ps1
@@ -0,0 +1,80 @@
+#-------------------------------------------------------------------------------------------------------------------------------------------------#
+#          _             _                _            _            _   _         _                 _          _          _             _         #
+#         / /\          /\_\             /\ \         /\ \         /\_\/\_\ _    / /\              /\ \       /\ \       /\ \     _    /\ \       #
+#        / /  \        / / /         _   \_\ \       /  \ \       / / / / //\_\ / /  \             \_\ \      \ \ \     /  \ \   /\_\ /  \ \      #
+#       / / /\ \       \ \ \__      /\_\ /\__ \     / /\ \ \     /\ \/ \ \/ / // / /\ \            /\__ \     /\ \_\   / /\ \ \_/ / // /\ \_\     #
+#      / / /\ \ \       \ \___\    / / // /_ \ \   / / /\ \ \   /  \____\__/ // / /\ \ \          / /_ \ \   / /\/_/  / / /\ \___/ // / /\/_/     #
+#     / / /  \ \ \       \__  /   / / // / /\ \ \ / / /  \ \_\ / /\/________// / /  \ \ \        / / /\ \ \ / / /    / / /  \/____// / / ______   #
+#    / / /___/ /\ \      / / /   / / // / /  \/_// / /   / / // / /\/_// / // / /___/ /\ \      / / /  \/_// / /    / / /    / / // / / /\_____\  #
+#   / / /_____/ /\ \    / / /   / / // / /      / / /   / / // / /    / / // / /_____/ /\ \    / / /      / / /    / / /    / / // / /  \/____ /  #
+#  / /_________/\ \ \  / / /___/ / // / /      / / /___/ / // / /    / / // /_________/\ \ \  / / /   ___/ / /__  / / /    / / // / /_____/ / /   #
+# / / /_       __\ \_\/ / /____\/ //_/ /      / / /____\/ / \/_/    / / // / /_       __\ \_\/_/ /   /\__\/_/___\/ / /    / / // / /______\/ /    #
+# \_\___\     /____/_/\/_________/ \_\/       \/_________/          \/_/ \_\___\     /____/_/\_\/    \/_________/\/_/     \/_/ \/___________/     #
+#                            _              _        _            _       _                _        _    _        _                               #
+#                           / /\      _    /\ \     /\ \         / /\    / /\             /\ \     /\ \ /\ \     /\_\                             #
+#                          / / /    / /\   \ \ \    \_\ \       / / /   / / /             \ \ \   /  \ \\ \ \   / / /                             #
+#                         / / /    / / /   /\ \_\   /\__ \     / /_/   / / /              /\ \_\ / /\ \ \\ \ \_/ / /                              #
+#                        / / /_   / / /   / /\/_/  / /_ \ \   / /\ \__/ / /              / /\/_// / /\ \ \\ \___/ /                               #
+#                       / /_//_/\/ / /   / / /    / / /\ \ \ / /\ \___\/ /      _       / / /  / / /  \ \_\\ \ \_/                                #
+#                      / _______/\/ /   / / /    / / /  \/_// / /\/___/ /      /\ \    / / /  / / /   / / / \ \ \                                 #
+#                     / /  \____\  /   / / /    / / /      / / /   / / /       \ \_\  / / /  / / /   / / /   \ \ \                                #
+#                    /_/ /\ \ /\ \/___/ / /__  / / /      / / /   / / /        / / /_/ / /  / / /___/ / /     \ \ \                               #
+#                    \_\//_/ /_/ //\__\/_/___\/_/ /      / / /   / / /        / / /__\/ /  / / /____\/ /       \ \_\                              #
+#                        \_\/\_\/ \/_________/\_\/       \/_/    \/_/         \/_______/   \/_________/         \/_/                              #
+#                                                                                                                                                 #
+#-------------------------------------------------------------------------------------------------------------------------------------------------#
+# Disclaimer:                                                                                                                                     #
+#                                                                                                                                                 #
+# This script comes with no guarantees. The cmdlets in this script functioned as is on the moment of creating the script.                         #
+# It is possible that during the lifecycle of the product this script is intended for, updates were performed to the systems and the script       #
+# might not, or might to some extent, no longer function.                                                                                         #
+#                                                                                                                                                 #
+# Therefor, I would suggest running the script in a test environment first, cmdlet per cmdlet, before effectively running it in production        #
+# environments.                                                                                                                                   #
+#                                                                                                                                                 #
+# Created by Leon Moris                                                                                                                           #
+# Website: www.switchtojoy.be                                                                                                                     #
+# Github: https://github.com/Joy-Leon                                                                                                             #
+#-------------------------------------------------------------------------------------------------------------------------------------------------#
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------#
+# Declared functions.
+
+function func_logging {   
+    param ($String) 
+    func_writeok $string
+    Start-Sleep -Seconds 1 
+    return "[{0:dd/MM/yy} {0:HH:mm:ss}] $String" -f (Get-Date)  | Out-File $logfile -append
+}
+function func_writeok {
+    param ($string)
+    write-host ""
+    write-host $string -f green
+    Start-Sleep -Seconds 1
+}
+function func_writenok {
+    param ($string)
+    write-host ""
+    write-host $string -f red
+    Start-Sleep -Seconds 1
+}
+
+function func_directory {
+    param($Path)
+    if (!(Test-Path $Path)) {
+        New-Item -ItemType Directory -Path $Path | out-null
+    }
+}
+
+function func_checkvariable {
+    param ($variable, $filename, $name)
+    if ($null -eq $variable){
+        func_writenok "An error has occurred with gathering data from the file $FILENAME"
+        func_writenok "Some data is missing. Please check your configuration and retry."
+        break 
+    } Else {
+        func_logging "The $name is $($variable)"
+    }
+}
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------#
